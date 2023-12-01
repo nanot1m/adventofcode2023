c019b54831117b025b0ddd34391a18aa5d706b5b
Timur Khazamov <bboytx@gmail.com>
2023-12-01 06:58:42 +0100

    Day 1. JS

--- js/solutions/1.js
+++ js/solutions/1.js
@@ -1,27 +1,81 @@
 // @ts-check
 
-import { it, sum } from "../modules/itertools.js"
+import { it } from "../modules/itertools.js"
 import { t } from "../modules/parser.js"
 
-export const useExample = true
+export const useExample = false
 
 export const exampleInput = `\
 `
 
 /** @typedef {ReturnType<typeof parseInput>} InputType */
 
-export const parseInput = t.arr(t.int()).parse
+export const parseInput = t.arr(t.str()).parse
 
 /**
  * @param {InputType} input
  */
 export function part1(input) {
-  return it(input).groupsOf(3).map(sum).max()
+  return it(input)
+    .map((s) => s.replace(/\D/g, ""))
+    .map((x) => x.at(0) + x.at(-1))
+    .map(Number)
+    .sum()
+}
+
+const nums = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+]
+
+/**
+ *
+ * @param {string} s
+ * @returns
+ */
+function findNumbersInString(s) {
+  let left = 0
+
+  const result = []
+
+  while (left < s.length) {
+    if (/\d/.test(s[left])) {
+      result.push(Number(s[left]))
+      left++
+      continue
+    }
+
+    let cur = s[left]
+    let right = left + 1
+    while (right < s.length && right - left < 6) {
+      cur += s[right]
+      const idx = nums.indexOf(cur)
+      if (idx >= 0) {
+        result.push(idx + 1)
+        left = right - 1
+        break
+      }
+      right++
+    }
+    left++
+  }
+
+  return result
 }
 
 /**
  * @param {InputType} input
  */
 export function part2(input) {
-  return it(input).groupsOf(3).map(sum).max()
+  return it(input)
+    .map(findNumbersInString)
+    .map((x) => Number(`${x.at(0)}${x.at(-1)}`))
+    .sum()
 }
