25545d953136c9aa530ec8e0cda9454a13b1bd8e
Timur Khazamov <teimuraz@miro.com>
2023-12-13 09:27:47 +0100

    Day 13. JS. Part 2

--- js/solutions/13.js
+++ js/solutions/13.js
@@ -1,12 +1,10 @@
 // @ts-check
 
-import { V } from "../modules/index.js"
 import { it } from "../modules/itertools.js"
 import { Map2d, parseMap2d } from "../modules/map2d.js"
 import { t } from "../modules/parser.js"
-import { Range } from "../modules/range.js"
 
-export const useExample = true
+export const useExample = false
 
 export const exampleInput = `\
 #.##..##.
@@ -34,7 +32,7 @@ export const parseInput = t.arr(t.str()).map((lines) => lines.map(parseMap2d)).p
  */
 export function part1(input) {
 	return it(input)
-		.map(findReflectionPosition)
+		.map((p) => findReflectionPosition(p))
 		.map((p) => p.x + p.y * 100)
 		.sum()
 }
@@ -45,47 +43,31 @@ export function part1(input) {
 export function part2(input) {
 	let total = 0
 
-	const ogReflections = it(input).map(findReflectionPosition).toArray()
-
 	let idx = 0
 	for (const m of input) {
-		const widthCenter = m.width / 2
-		const heightCenter = m.height / 2
-		const ogReflection = ogReflections[idx]
-		const ogX = ogReflection.x
-		const ogY = ogReflection.y
-		let ignoreColumnsRange = new Range(-1, -1)
-		let ignoreRowsRange = new Range(-1, -1)
-		if (ogX > 0) {
-			if (ogX < widthCenter) {
-				ignoreColumnsRange = new Range(ogX * 2 - 1, m.width - 1)
-			} else {
-				ignoreColumnsRange = new Range(0, ogX * 2 - m.width + 1)
-			}
-		} else {
-			if (ogY < heightCenter) {
-				ignoreRowsRange = new Range(ogY * 2 - 1, m.height - 1)
-			} else {
-				ignoreRowsRange = new Range(0, ogY * 2 - m.height + 1)
-			}
-		}
-
-		console.log({ ignoreColumnsRange, ignoreRowsRange }, idx)
+		const og = findReflectionPosition(m)
 
 		let found = false
-		for (const { pos, value } of m) {
-			if (ignoreColumnsRange.includes(pos[0]) || ignoreRowsRange.includes(pos[0])) continue
+		outer: for (const { pos, value } of m) {
 			m.set(pos, value === "#" ? "." : "#")
-			const p = findReflectionPosition(m)
-			if (p.x > 0 || p.y > 0) {
+			const md = findReflectionPosition(m, og)
+
+			const points = []
+			if (md.x > 0 && md.y > 0) {
+				points.push({ x: md.x, y: 0 }, { x: 0, y: md.y })
+			} else if (md.x > 0 || md.y > 0) {
+				points.push(md)
+			}
+
+			for (const p of points) {
 				found = true
 				total += p.x + p.y * 100
-				break
-			} else {
-				m.set(pos, value)
+				break outer
 			}
+			m.set(pos, value)
 		}
 		if (!found) {
+			console.log(m.toString())
 			throw new Error("Smudge not found in map " + idx)
 		}
 		idx++
@@ -97,51 +79,53 @@ export function part2(input) {
 /**
  * @param {Map2d<string>} m
  */
-function findReflectionPosition(m) {
+function findReflectionPosition(m, og = { x: 0, y: 0 }) {
 	const columns = it(m.columns())
 		.map(([, cs]) => Array.from(cs, (c) => c.value).join(""))
 		.toArray()
-	const centerX = findEvenPalindromeCenter(columns)
+	const centerX = findEvenPalindromeCenter(columns, [og.x - 1])
 
 	const rows = it(m.lines())
 		.map(([, cs]) => Array.from(cs, (c) => c.value).join(""))
 		.toArray()
-	const centerY = findEvenPalindromeCenter(rows)
+	const centerY = findEvenPalindromeCenter(rows, [og.y - 1])
 
 	const x = centerX === null ? 0 : centerX + 1
 	const y = centerY === null ? 0 : centerY + 1
-
-	if (x > 0 && y > 0) {
-		return { x: 0, y: 0 }
-		// console.log(m.toString({ valToString: (v) => (v === "#" ? "🔳" : "⬜️") }))
-		// console.log({ x, y })
-		// throw new Error("Both x and y are positive")
-	}
 
 	return { x, y }
 }
 
 /**
  * @param {string[]} strs
+ * @param {number[]} skips
  *
  * @returns {number | null}
  */
-function findEvenPalindromeCenter(strs) {
-	let j = 0
+function findEvenPalindromeCenter(strs, skips = []) {
+	let longest = [0, 0]
+	let longestIdx = -1
+	let right = 0
+	let found = false
 	for (let i = 0; i < strs.length - 1; i++) {
-		const result = expand(i)
-		if (j === strs.length || result === -1) {
-			return i
+		if (skips.includes(i)) continue
+		const left = expand(i)
+		if (right === strs.length || left === -1) {
+			found = true
+			if (right - left > longest[1] - longest[0]) {
+				longest = [left + 1, right - 1]
+				longestIdx = i
+			}
 		}
 	}
-	return null
+	return found ? longestIdx : null
 
-	function expand(/** @type {number} */ i) {
-		j = i + 1
-		while (i >= 0 && j < strs.length && strs[i] === strs[j]) {
-			i--
-			j++
+	function expand(/** @type {number} */ left) {
+		right = left + 1
+		while (left >= 0 && right < strs.length && strs[left] === strs[right]) {
+			left--
+			right++
 		}
-		return i
+		return left
 	}
 }
